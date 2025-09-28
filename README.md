0a85284fa098538e7f731567042b9a6594e81c62
engelsimmanuel <engelmmanuel@gmail.com>
2025-09-28 15:55:13 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# protobuf_experiment
+
+Sample Flutter app that transmits data over Socket IO using protobuf
---
+++ android/app/src/main/kotlin/trade/sky/protobuf_experiment/MainActivity.kt
@@ -0,0 +1,5 @@
+package trade.sky.protobuf_experiment
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
